45dc198953577fcfb8c606fe76f404a1729f3ff4
Denis Novikov <voothi@gmail.com>
2019-04-26 00:37:45 +0300

    не работает, функции

--- lesson1/phw/ex1/script/script.js
+++ lesson1/phw/ex1/script/script.js
@@ -1,22 +1,32 @@
 
 let tCelsius = null;
 let tFahrenheit = null;
+let ScaleOfTemperature = "tCelsius";
 
-tCelsius = prompt("Введите температуру в градусах по Цельсию: ");
-
-console.log(typeof (tCelsius));
-
-if (typeof (tCelsius) == 'number') {
-    console.log("is number");
+function tInputUser(ScaleOfTemperature) {
+    if (ScaleOfTemperature == "tCelsius") {
+        tCelsiusParam = prompt("Введите температуру в градусах по Цельсию: ");
+    }
+    return tCelsiusParam;
 }
 
-// Проверка ввода - только
-// Пограничные значения
-
-if (tCelsius == "") {
-    alert("Вы не ввели значение - присвоено 0 в градусах по Цельсию");
-    tCelsius = 0;
+function tSetDefault(tCelsiusParam) {
+    if (tCelsiusParam == "") {
+        alert("Вы не ввели значение - присвоено 0 в градусах по Цельсию");
+        tCelsiusParam = 0;
+    }
 }
 
-tFahrenheit = (9 / 5) * tCelsius + 32;
-alert("Температура " + tCelsius + " градусов по Цельсию равна " + tFahrenheit + " градусам по Фаренгейту");
+function tConvertCelsiusToFahrenheit(tCelsiusParam) {
+    return tFahrenheit = (9 / 5) * tCelsiusParam + 32;
+}
+
+function tOutputUser(tCelsiusParam, tFahrenheitParam) {
+    return alert("Температура " + tCelsiusParam + " градусов по Цельсию равна " + tFahrenheitParam + " градусам по Фаренгейту");
+}
+
+
+tCelsius = tInputUser(ScaleOfTemperature);
+tCelsius = tSetDefault(tCelsius);
+tFahrenheit = tConvertCelsiusToFahrenheit(tCelsius);
+tOutputUser(tCelsius, tFahrenheit);
