ecf9c07fc1011943b09587b51071fdb25bac9837
Denis Novikov <voothi@gmail.com>
2019-04-29 21:20:52 +0300

    пароль, сравнение условие

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,5 +1,1 @@
-// Регулярные выражения
-let regex = /(https?)(:\/\/)([a-z]+)(.)([a-z]{2,}])/gm;
-let str = "asdffdew https://google.ru jklasqwer";
-let match = regex.test(str);
-console.log(match);
+// Пример с паролем
