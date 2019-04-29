b6f79332a8d83b0ec167d1c89f25c5fbeb90a9c2
Denis Novikov <voothi@gmail.com>
2019-04-29 20:42:29 +0300

    полная форма

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,8 +1,10 @@
-// Неполная форма ветвления
-let answer = prompt("Загадка, ответ ");
-answer = answer.toLowerCase();
-if (answer == ("елка" || "ёлка")) {
-    alert("Угадали");
+// Полная (2 ветки) форма ветвления
+let user1score = 1
+let user2score = 2
+if (user1score > user2score) {
+    alert("Win user1");
+} else {
+    alert("Win user2");
 }
 
 // можно улучшить с помощью массива - проходить по массиву
