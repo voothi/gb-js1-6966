483d1f9cde478fd6ba5a199ed578484d42871b9d
Denis Novikov <voothi@gmail.com>
2019-04-29 21:50:08 +0300

    a+b and alarm

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,18 +1,7 @@
 // Case
 let
 // function declaration
-function name(params) {
-    let dayNum = new Date().getDay();
-    let day = '';
-    switch (day) {
-        case 1:
-            day = "Пн";
-            break;
-        case 2:
-            day = "Вт";
-            break;
-        default:
-            console.log("Что это?")
-            break;
-    }
+function sum(a, b) {
+    return a + b;
+    
 }
