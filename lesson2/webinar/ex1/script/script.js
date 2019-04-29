3ffd8edd4d6b9f6f7489f921c9bb7bf41e3f5a7a
Denis Novikov <voothi@gmail.com>
2019-04-29 20:52:57 +0300

    sum

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,8 +1,4 @@
-// Полная (2 ветки) форма ветвления
-let user1score = 1
-let user2score = 2
-if (user1score > user2score) {
-    alert("Win user1");
-} else if {
-    alert("Win user2"); // можно попасться, если отступ вровень, т.к. это else
+// Фиг. ск. содержит в себе тело функции
+function () {
+    const sum = num
 }
