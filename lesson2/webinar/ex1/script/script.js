d56e7cc46f89517618f0ed4e1b81818f119ad9b9
Denis Novikov <voothi@gmail.com>
2019-04-29 21:44:27 +0300

    function declaration

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,13 +1,18 @@
 // Case
-let day = prompt("Введите день недели");
-switch (day) {
-    case "пн":
-        console.log("Это Пн");
-    break;
-    case "вт":
-        console.log("Это Вт");
-    break;
-    default:
-        console.log("Что это?")
-    break;
+let
+// function declaration
+function name(params) {
+    let dayNum = new Date().getDay();
+    let day = '';
+    switch (day) {
+        case 1:
+            day = "Пн";
+            break;
+        case 2:
+            day = "Вт";
+            break;
+        default:
+            console.log("Что это?")
+            break;
+    }
 }
