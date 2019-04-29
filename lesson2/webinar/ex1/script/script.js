0d6ea0e0974b6c3a491829fc44ff7ed777601e7a
Denis Novikov <voothi@gmail.com>
2019-04-29 21:11:08 +0300

    regex

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,4 +1,5 @@
-// Фиг. ск. содержит в себе тело функции
-function () {
-    const sum = num
-}
+// Регулярные выражения
+let regex = /(https?)(:\/\/)([a-z]+)(.)([a-z]{2,}])/gm;
+let str = "asdffdew https://google.ru jklasqwer";
+let match = regex.test(str);
+console.log(match);
