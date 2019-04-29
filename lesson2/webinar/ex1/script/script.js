9e55eddf4ad189c56ba0376a3b85e716a205897c
Denis Novikov <voothi@gmail.com>
2019-04-29 20:48:57 +0300

    else if () test

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,6 +1,8 @@
 // Полная (2 ветки) форма ветвления
 let user1score = 1
-let user2score = 1
-if (user1score > user2score)
+let user2score = 2
+if (user1score > user2score) {
     alert("Win user1");
-alert("Win user2"); // можно попасться, если отступ вровень, т.к. это else
+} else if {
+    alert("Win user2"); // можно попасться, если отступ вровень, т.к. это else
+}
