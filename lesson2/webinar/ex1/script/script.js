24b7add7f59606a6279ddd9b11f5c548875c9319
Denis Novikov <voothi@gmail.com>
2019-04-30 08:21:04 +0300

    fix ] regex var

--- lesson2/webinar/ex1/script/script.js
+++ lesson2/webinar/ex1/script/script.js
@@ -1,5 +1,5 @@
 // Регулярные выражения
-let regex = /(https?)(:\/\/)([a-z]+)(.)([a-z]{2,}])/gm;
+let regex = /(https?)(:\/\/)([a-z]+)(.)([a-z]{2,})/gm;
 let str = "asdffdew https://google.ru jklasqwer";
 let match = regex.test(str);
 console.log(match);
