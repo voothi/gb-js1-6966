30f1651994b60f61dcad45fb1199c0a1106182bd
Denis Novikov <voothi@gmail.com>
2019-05-07 16:08:05 +0300

    задания

---
+++ lesson3/phw/ex3/script/script.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
---
+++ lesson3/phw/ex4/script/script.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
