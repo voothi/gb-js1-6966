a88da46ac3a82de289eead0466e58317661046f0
Denis Novikov <voothi@gmail.com>
2019-05-07 19:47:23 +0300

    готово ex1

---
+++ lesson3/phw/ex1/script/script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 10; i++) {
+    if (i === 0)
+        console.log(`${i} - это ноль`);
+    else if (i % 2 === 0)
+        console.log(`${i} - это четное число`);
+    else
+        console.log(`${i} - это нечетное число`);
+}
