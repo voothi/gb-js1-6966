aa20623b577e500f9e9ad44813b389ba31f1a712
Denis Novikov <voothi@gmail.com>
2019-05-14 15:28:57 +0300

    userrole

--- lesson4/webinar/ex2/script/script.js
+++ lesson4/webinar/ex2/script/script.js
@@ -4,22 +4,21 @@
 function Post(author, text) {
     this.author = author;
     this.text = text;
-}
-
-Post.prototype.show = function() {
-    alert(this.author + " say: " + this.text);
+    this.show = function() {
+        alert(this.author + " say: " + this.text);
+    }
 }
 
 const post1 = new Post("Admin", "lorem");
 
-// console.log(typeof post1);
+console.log(typeof post1);
 
 // console.log(post1);
 console.log(post1.author);
 console.log(post1.text);
 post1.show();
 
-// const post2 = new Post("User", "Hello");
-// console.log(post2.author);
-// console.log(post2.text);
-// post2.show();
+const post2 = new Post("User", "Hello");
+console.log(post2.author);
+console.log(post2.text);
+post2.show();
--- lesson4/webinar/ex3/script/script.js
+++ lesson4/webinar/ex3/script/script.js
@@ -1,25 +1,10 @@
 "use strict";
 
-// Функция конструктор
-function Post(author, text) {
-    this.author = author;
-    this.text = text;
+function UserRole () {
+    this.rights = ['create', 'edit'];
 }
 
-Post.prototype.show = function() {
-    alert(this.author + " say: " + this.text);
-}
+const userRole1 = new UserRole();
+debugger;
 
-const post1 = new Post("Admin", "lorem");
-
-// console.log(typeof post1);
-
-// console.log(post1);
-console.log(post1.author);
-console.log(post1.text);
-post1.show();
-
-// const post2 = new Post("User", "Hello");
-// console.log(post2.author);
-// console.log(post2.text);
-// post2.show();
+console.log(userRole1.__proto__ === UserRole.prototype);
