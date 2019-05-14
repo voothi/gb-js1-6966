0c07aa99cb04d951b8927170159613b91648c19f
Denis Novikov <voothi@gmail.com>
2019-05-14 22:32:27 +0300

    решение ex10 бродилка

---
+++ lesson4/webinar/ex10/mover.js
@@ -0,0 +1,57 @@
+let mover = {
+    /**
+     * Получает и отдает направление от пользователя.
+     * @returns {int} Возвращаем направление, введенное пользователем.
+     */
+    getDirection() {
+        // Доступные значения ввода.
+        const availableDirections = [-1, 2, 4, 6, 8];
+
+        while (true) {
+            // Получаем от пользователя направление.
+            let direction = parseInt(prompt('Введите число (2, 4, 6 или 8), куда вы хотите переместиться, "Отмена" для выхода.'));
+            if (isNaN(direction)) {
+                return null;
+            }
+            // Если направление не одно из доступных, то сообщаем что надо ввести корректные данные
+            // и начинаем новую итерацию.
+            if (!availableDirections.includes(direction)) {
+                alert('Для перемещения необходимо ввести одно из чисел 2, 4, 6 или 8.');
+                continue;
+            }
+
+            // Если пользователь ввел корректное значение - отдаем его.
+            return direction;
+        }
+    },
+
+    /**
+     * Отдает следующую точку в которой будет находиться пользователь после движения.
+     * @param {int} direction Направление движения игрока.
+     * @returns {{x: int, y: int}} Следующая позиция игрока.
+     */
+    getNextPosition(direction) {
+        // Следующая точка игрока, в самом начале в точке будут текущие координаты игрока.
+        const nextPosition = {
+            x: player.x,
+            y: player.y,
+        };
+        // Определяем направление и обновляем местоположение игрока в зависимости от направления.
+        switch (direction) {
+            case 2:
+                nextPosition.y++;
+                break;
+            case 4:
+                nextPosition.x--;
+                break;
+            case 6:
+                nextPosition.x++;
+                break;
+            case 8:
+                nextPosition.y--;
+                break;
+        }
+
+        return nextPosition;
+    },
+};
