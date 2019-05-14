24aa94191117b64b79b98d14e9219d8b2197052c
Denis Novikov <voothi@gmail.com>
2019-05-14 15:34:47 +0300

    hellp

--- lesson4/webinar/ex3/script/script.js
+++ lesson4/webinar/ex3/script/script.js
@@ -4,7 +4,8 @@ function UserRole () {
     this.rights = ['create', 'edit'];
 }
 
+UserRole.prototype.hello = "hello";
+
 const userRole1 = new UserRole();
-debugger;
 
 console.log(userRole1.__proto__ === UserRole.prototype);
