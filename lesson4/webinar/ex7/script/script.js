b93eb57e371182557c1bd9489aebf92f0bdbf4f7
Denis Novikov <voothi@gmail.com>
2019-05-14 17:46:26 +0300

    не работает

--- lesson4/webinar/ex7/script/script.js
+++ lesson4/webinar/ex7/script/script.js
@@ -1,18 +1,22 @@
 "use strict";
 
 class User {
+    url = null;
+
     constructor(name, surname) {
         this.name = name;
         this.surname = surname;
     }
 
-    get fullName() {
-        return this.name + " " + this.surname;
+    set vkUrl(url) {
+        if (/^https?.*/gm.test(url)) {
+            this.url = url;
+        } else {
+            throw new Error("Passed invalid URL value: " + url);
+        }
     }
 }
 
 const user1 = new User("Denis", "Novikov");
-console.log(user1.name);
-console.log(user1.surname);
-console.log(user1.fullName);
-
+user1.vkUrl = 'https://vk.com/test';
+console.log(user1.vkUrl);
