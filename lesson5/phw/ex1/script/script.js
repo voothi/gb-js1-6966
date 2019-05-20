f38e1fb75636527d573b02a9b1ebcbbe7c8e71fe
Denis Novikov <voothi@gmail.com>
2019-05-20 11:30:27 +0300

    ex1 перенос файлов

--- lesson5/phw/ex1/script/script.js
+++ lesson5/phw/ex1/script/script.js
@@ -1,2 +1,22 @@
 "use strict"
 
+let modal = document.querySelector(".modal");
+let button_show_modal = document.querySelector(".button_show_modal");
+let button_close = document.querySelector(".button_close");
+
+
+button_show_modal.addEventListener("click", function () {
+    modal.style.display = "block";
+    modal.classList.add("magictime", "puffIn");
+});
+
+button_close.addEventListener("click", function () {
+    modal.classList.remove("magictime", "puffIn");
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
