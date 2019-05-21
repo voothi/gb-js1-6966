d624d03cc4deece2dacd5d5177e79ec3f79c18f9
Denis Novikov <voothi@gmail.com>
2019-05-21 17:27:51 +0300

    вставили img

--- lesson6/phw/ex1/script/script.js
+++ lesson6/phw/ex1/script/script.js
@@ -1,13 +1,8 @@
 "use strict"
 
-let slider = document.querySelector(".slider");
-
-let imgstr = "";
 for (let key in imgData) {
-    let a = document.createElement("img");
-    a.setAttribute("src", "http://ya.ru/1.img");
-    console.dir(a.src);
-    console.log(a.src);
-    console.log(typeof (a));
-    imgstr += document.createElement("img") + imgData[key].url;
+    let insertImg = document.createElement("img");
+    insertImg.setAttribute("src", imgData[key].url);
+    insertImg.setAttribute("alt", "");
+    document.querySelector(".slider").appendChild(insertImg);
 }
