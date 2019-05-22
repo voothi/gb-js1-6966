c66a8d9bd2790ae3d88ec927cdf94694741db807
Denis Novikov <voothi@gmail.com>
2019-05-22 22:13:07 +0300

    load ok

--- lesson6/phw/ex1/script/script.js
+++ lesson6/phw/ex1/script/script.js
@@ -3,15 +3,21 @@ let indexMax = imgData.length - 1;
 let indexCurrent = 0;
 
 for (var i = 0; i < imgData.length; i++) {
-    // создать элемент img
     let insertImg = document.createElement("img");
     insertImg.setAttribute("src", imgData[i].url);
     insertImg.classList.add("hidden");
-    // добавить в слайдер
     document.querySelector(".slider").appendChild(insertImg);
 }
 
-button = document.querySelector(".btn-right");
-button.addEventListener("click", function (event) {
-    insertImg.classList.add("hidden");
+window.addEventListener("load", function (event) {
+    document
+        .querySelector(".slider")
+        .querySelectorAll("img")[indexCurrent]
+        .classList.remove("hidden");
+});
+
+
+let buttonRight = document.querySelector(".btn-right");
+buttonRight.addEventListener("click", function (event) {
+
 });
